ccb02e3da77eb136c7548cc5e0e767911ceeb246
Chaloemchai Aintharaphithak <ChaleamchaiSt@gmail.com>
2021-09-04 19:22:32 +0700

    first commit'not complete'

---
+++ screens/MapScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import tw from 'tailwind-react-native-classnames'
+
+const MapScreen = () => {
+  return (
+    <View>
+      <Text
+      style={{ flex: 1,}}>Here is the map stuff...</Text>
+    </View>
+  );
+};
+
+export default MapScreen;
+
+const styles = StyleSheet.create({});
